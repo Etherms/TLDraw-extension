d8a1427de3247acf5ba8b875167abe13cf11261f
Etherms <edsonhermosa17@outlook.com>
2023-10-04 11:04:10 +0800

    Added Initial window feature with webview

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 const createdTabs: any[] = [];
+const windows: any[] = [];
 const countedTabs: number[] = [];
 
+//Function to get what Tab Number is available
 function findNextAvailableTabNumber(tabNumbers: number[]):number{
     if (tabNumbers.length === 0){
         return 1;
@@ -26,6 +28,27 @@ ext.runtime.onExtensionClick.addListener(async () => {
         index: nextTabNumber - 1,
         closable: true,
     })
+
+    let newWindow = await ext.windows.create({
+        title: `TLDraw - #${nextTabNumber}`,
+        icon: '../src/icons/icon-1024.png',
+        vibrancy: false,
+        frame: true,
+        minWidth: 650,
+        minHeight: 460,
+        aspectRatio: 650 /460
+    })
+
+    let myWindowSize = await ext.windows.getContentSize(newWindow.id)
+    let myWebview = await ext.webviews.create({
+        window:newWindow,
+        bounds: { x: 0, y: 0, width: myWindowSize.width, height: myWindowSize.height },
+        autoResize: { width: true, height: true }
+    })
+
+    await ext.webviews.loadURL(myWebview.id, 'https://www.tldraw.com')
+    
+    windows.push(newWindow);
     createdTabs.push(newTab);
     countedTabs.push(nextTabNumber);
 });
