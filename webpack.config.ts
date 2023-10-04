b96cc3fad250070f18603709f0ff81a45f3d00b2
Etherms <edsonhermosa17@outlook.com>
2023-10-04 22:33:39 +0800

    Changed webpack to typescript and changed tsconfig target to ES6

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import * as CopyPlugin from 'copy-webpack-plugin';
+import * as path from 'path';
+import { Configuration } from 'webpack';
+
+const webpackConfig: Configuration = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development', 
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/icons', to: 'icons' },
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        { from: 'src/index.html', to: 'index.html' },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+};
+
+export default webpackConfig;
